b02ccc2f6806d4f2e3a723d5d7814fba0c273113
Andrey Sobolev <haiodo@gmail.com>
2020-01-10 18:35:40 +0700

    Fix vet issue
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-CUR_DIR=$(dirname "$0")
 TEST_LIST=$(go list ./... | grep -v -e "sample" | grep -e "^github")
 # shellcheck disable=SC2086
 gotestsum --junitfile ~/junit/unit-tests.xml -- -short $TEST_LIST
