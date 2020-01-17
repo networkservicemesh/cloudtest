2c33510ba5cd761fca11aa737d1bd8e787a4539d
Andrey Sobolev <haiodo@gmail.com>
2020-01-17 23:09:56 +0700

    Fix go-header error
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- examples/kind/tests/sample_test.go
+++ examples/kind/tests/sample_test.go
@@ -1,4 +1,6 @@
-// Copyright (c) 2020 Cisco and/or its affiliates.
+// Copyright (c) 2019-2020 Cisco Systems, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +13,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 
 package sample
 
