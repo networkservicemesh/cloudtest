networkservicemesh/networkservicemesh

Global 'only-run' test restriction for cloudtest config
Signed-off-by: Alexander Yustus <alexander.yustus@xored.com>

test/cloudtest/pkg/config/config.go
@@ -85,7 +85,8 @@ type CloudTestConfig struct {
 		Enabled  bool  `yaml:"enabled"`  // A way to disable printing of statistics
 	} `yaml:"statistics"` // Statistics options
 
-	ShuffleTests bool `yaml:"shuffle-enabled"` // Shuffle tests before assignement
+	ShuffleTests bool     `yaml:"shuffle-enabled"` // Shuffle tests before assignement
+	OnlyRun      []string `yaml:"only-run"`        // If non-empty, only run the listed tests
 }
 
 // NewCloudTestConfig - creates a test config with some default values specified.

networkservicemesh/cloudtest

pkg/config/config.go
@@ -108,7 +108,8 @@ type CloudTestConfig struct {
 		Enabled  bool  `yaml:"enabled"`  // A way to disable printing of statistics
 	} `yaml:"statistics"` // Statistics options
 
-	ShuffleTests bool `yaml:"shuffle-enabled"` // Shuffle tests before assignment
+	ShuffleTests bool     `yaml:"shuffle-enabled"` // Shuffle tests before assignment
+	OnlyRun      []string `yaml:"only-run"`        // If non-empty, only run the listed tests
 }
 
 // NewCloudTestConfig - creates a test config with some default values specified.
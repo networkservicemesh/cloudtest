1e4c9d6e5f85001ad011b5e7d3f0d3098255ab1d
Alexander Yustus <alexander.yustus@xored.com>
2019-10-22 13:47:14 +0700

    Global 'only-run' test restriction for cloudtest config
    Signed-off-by: Alexander Yustus <alexander.yustus@xored.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -108,7 +108,8 @@ type CloudTestConfig struct {
 		Enabled  bool  `yaml:"enabled"`  // A way to disable printing of statistics
 	} `yaml:"statistics"` // Statistics options
 
-	ShuffleTests bool `yaml:"shuffle-enabled"` // Shuffle tests before assignment
+	ShuffleTests bool     `yaml:"shuffle-enabled"` // Shuffle tests before assignment
+	OnlyRun      []string `yaml:"only-run"`        // If non-empty, only run the listed tests
 }
 
 // NewCloudTestConfig - creates a test config with some default values specified.
