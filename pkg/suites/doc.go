9d022e4cde87ad043583fadff382c3b203f4c0b7
Vladimir Popov <vladimir.popov@xored.com>
2021-02-16 12:28:42 +0700

    Fix linter issues
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

--- pkg/suites/doc.go
+++ pkg/suites/doc.go
@@ -14,5 +14,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package parse provides tools to work with go test suites
+// Package suites provides tools to work with go test suites
 package suites
